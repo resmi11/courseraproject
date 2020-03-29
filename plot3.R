aa06808b63fb2ba61563c6c7602ff11ae58d9960
resmi11 <62787584+resmi11@users.noreply.github.com>
2020-03-29 18:41:25 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,7 +6,10 @@ data$DateTime <- as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %X")
 data <- data[,c(ncol(data), 3:(ncol(data)-1))]
 data <- data[!is.na(data$DateTime) & data$DateTime >= as.POSIXct("2007-02-01") & data$DateTime < as.POSIXct("2007-02-03"), ]
 
-png("plot1.png")
-hist(data$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+png("plot3.png")
+with(data, plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
+with(data, lines(DateTime, Sub_metering_2, type="l", col="red"))
+with(data, lines(DateTime, Sub_metering_3, type="l", col="blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"),lty=1)
 dev.off() 
- 
+  
